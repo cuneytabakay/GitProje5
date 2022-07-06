540975bbe9957e1319496a188378caa282dff462
cuneytabakay <cuneytabakay@hotmail.com>
2022-07-06 19:23:02 +0300

    merhaba 1

---
+++ src/Gun01/Ogrenci1.java
@@ -0,0 +1,11 @@
+package Gun01;
+
+public class Ogrenci1 {
+    public static void main(String[] args) {
+        System.out.println("merhaba 1");
+
+        System.out.println("merhaba 2");
+
+
+    }
+}
